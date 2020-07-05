e6aa55bb81b3cf2df0d5b377e809e10f45bdbd57
alexaspalato <alexaspalato@gmail.com>
2020-07-05 06:55:40 +0200

    style

--- components/Column.js
+++
@@ -1,7 +0,0 @@
-/** @jsx jsx */
-import { jsx, Box } from "theme-ui"
-import React from "react"
-
-export const Column = ({ children }) => {
-  return <Box>{children}</Box>
-}
--- theme.js
+++ theme.js
@@ -1,13 +1,30 @@
 import { github, vsDark, shadesOfPurple } from "@code-surfer/themes"
+import { themes } from "mdx-deck"
 
 export default {
-  ...shadesOfPurple,
+  ...themes.condensed,
+
+  fonts: {
+    body: "Coda, cursive",
+    heading: '"Coda Caption", sans-serif',
+  },
+  colors: {
+    text: "#FAD000",
+    background: "#27264A",
+    link: "#fff",
+    pre: "#fff",
+    preBackground: "#051626",
+    code: "#fff",
+  },
 
   styles: {
+    h1: { color: "#6AD798" },
     li: {
       maxWidth: 960,
-      py: 15,
+      py: 10,
       listStyle: "none",
+      color: "#fff",
+      textAlign: "left",
     },
     a: {
       transition: "all .4s easeinout",
@@ -25,6 +42,12 @@ export default {
       //   bg: "red",
     },
     Header: {
+      textAlign: "center",
+    },
+    Slide: {
+      maxWidth: 1200,
+      mx: "auto",
+      px: 30,
       textAlign: "center",
     },
   },
